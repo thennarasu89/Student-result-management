c5bc04170bbbc9850dcc4e09c88900b437862bf5
thennarasu89 <thennarasusakthivel89@gmail.com>
2025-12-28 09:05:24 +0530

    Initial commit: Spring Boot backend setup

---
+++ student-result-management/src/main/java/com/kashvillan/studentresult/StudentResultManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.kashvillan.studentresult;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentResultManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentResultManagementSystemApplication.class, args);
+	}
+
+}
